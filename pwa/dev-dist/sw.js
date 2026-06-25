110fa11825653d49bccc4819b328885e99e4a551
Diogo <da.moutinho11@gmail.com>
2026-06-25 16:40:45 +0300

    feat: brief history table, approval endpoints, F-04 real data, APPROVE/DEFER/REJECT wired

--- pwa/dev-dist/sw.js
+++ pwa/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b696421e'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.54duvtreaig"
+    "revision": "0.di5g5uupmr8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
